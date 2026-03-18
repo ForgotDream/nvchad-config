2b3afa7d8ed8b6fa680159c78b110b920b9a8ff0
ForgotDream <mr.zyz@outlook.com>
2026-03-19 01:22:40 +0800

    new version of nvchad

---
+++ lua/mappings.lua
@@ -0,0 +1,18 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<leader>ta", ":CompetiTest add_testcase<CR>", { desc = "Add Testcase" })
+map("n", "<leader>te", ":CompetiTest edit_testcase<CR>", { desc = "Edit Testcase" })
+map("n", "<leader>td", ":CompetiTest delete_testcase<CR>", { desc = "Delete Testcase" })
+map("n", "<leader>tr", ":CompetiTest run<CR>", { desc = "Run" })
+map("n", "<leader>tn", ":CompetiTest run_no_compile<CR>", { desc = "Run With No Compile" })
+
+map("i", "<C-c>", "<ESC>")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+
+  {
+    "xeluxee/competitest.nvim",
+    dependencies = "MunifTanjim/nui.nvim",
+    config = function()
+      require("competitest").setup {
+        compile_command = {
+          cpp = { exec = "g++", args = { "$(FNAME)", "-o", "$(FNOEXT)", "-Wall", "--std=c++17", "-O2" }},
+        },
+
+        maximum_time = 2500,
+        view_output_diff = true,
+
+        testcases_directory = "/tmp/"
+      }
+    end,
+    ft = { "cpp" },
+  }
+}
